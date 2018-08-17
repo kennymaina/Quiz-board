6c32347174c7b2243c9d0b9f1efd3a80bafe9590
kennymaina <victormurigi@outlook.com>
2018-08-17 11:17:31 +0300

    change of h2, h3 colors in styles.css

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+function validate() {
+ var a= document.getElementByName("choice");
+ for (var a[i] = creating set of instructions) {
+   if (a[i].checked) {
+     if( a[i].value == "creating set of instruction" )
+        alert("your answer is correct");
+     else
+        alert("your answer is not correct");
+   }
+  }
+ }
