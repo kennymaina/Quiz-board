cf196ec1bbdff975dc3e13165bce72c1a67c4674
kennymaina <victormurigi@outlook.com>
2018-08-17 15:22:40 +0300

    addition of prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
+var answer = function(answer1, answer2,answer3, answer4) {
+  return answer1 + answer2 + answer3 + answer4;
+};
 
-function validate() {
- var a= document.getElementByName("choice");
- for (var a[i] = creating set of instructions) {
-   if (a[i].checked) {
-     if( a[i].value == "creating set of instruction" )
-        alert("your answer is correct");
-     else
-        alert("your answer is not correct");
-   }
-  }
- }
+var answer1 = parseInt(prompt("oops noo:"));
+var number2 = parseInt(prompt("oops noo:"));
+var answer3 = parseInt(prompt("you got this:"));
+var answer4 = parseInt(prompt("oops noo:"));
+var result = add(number1, number2, number4);
+alert(toobad);
